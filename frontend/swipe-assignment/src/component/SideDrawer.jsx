dc308cc0fa27233c9fa7f3e16040fdbcb70908dc
RITIKGITHUB18 <ritikasus911@gmail.com>
2024-11-25 03:39:11 +0530

    Swipe-assignment project requirement 1.0 done

---
+++ frontend/swipe-assignment/src/component/SideDrawer.jsx
@@ -0,0 +1,58 @@
+import { AppProvider } from "@toolpad/core/AppProvider";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import { PageContainer } from "@toolpad/core/PageContainer";
+import swipeLogo from "../assets/swipeLogo.png";
+
+// Sidebar icon
+import DescriptionRoundedIcon from "@mui/icons-material/DescriptionRounded";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import FileUploadRoundedIcon from "@mui/icons-material/FileUploadRounded";
+
+import { Outlet } from "react-router-dom";
+
+import theme from "../theme/theme";
+
+const NAVIGATION = [
+  {
+    kind: "header",
+    // title: "Requirements",
+  },
+  {
+    segment: "upload",
+    title: "Upload Invoice",
+    icon: <FileUploadRoundedIcon />,
+  },
+  {
+    segment: "invoice",
+    title: "Invoice",
+    icon: <DescriptionRoundedIcon />,
+  },
+  {
+    segment: "product",
+    title: "Product",
+    icon: <ShoppingCartIcon />,
+  },
+  {
+    segment: "customer",
+    title: "Customer",
+    icon: <PeopleAltIcon />,
+  },
+];
+
+export default function DashboardLayoutStruct({ children }) {
+  return (
+    <AppProvider
+      navigation={NAVIGATION}
+      theme={theme}
+      branding={{
+        logo: <img src={swipeLogo} alt="Swipe Logo" />,
+        title: "Swipe",
+      }}
+    >
+      <DashboardLayout>
+        <PageContainer>{children || <Outlet />}</PageContainer>
+      </DashboardLayout>
+    </AppProvider>
+  );
+}
